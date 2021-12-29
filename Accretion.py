607daff2f7e795032a65db0c7482bc603bd84182
Shikhar Mittal <60473059+shikharmittal04@users.noreply.github.com>
2021-12-29 12:50:00 +0530

    Update Accretion.py

--- Accretion.py
+++ Accretion.py
@@ -10,7 +10,7 @@ Ho=67.4
 Tγo=2.725
 Yp=0.245
 Ω_r=7.53e-3*Tγo**4/Ho**2
-ρdm=3*Ho**2/(8*np.pi*6.67e-11*(3.0857e19)**2)*(Ω_m-Ω_B)
+ρdm=3*Ho**2/(8*np.pi*6.67e-11*(3.0857e19)**2)*(Ω_m-Ω_B) #Dark matter density in SI units.
 
 def E2nu(E):
     return E*1e-6*1.6e-19*1e-9/(6.634e-34)
@@ -18,12 +18,15 @@ def E2nu(E):
 def nu2E(nu):
     return nu*6.634e-34/(1e-6*1.6e-19*1e-9)
 
-def H(Z):       
+
+def H(Z):       #Hubble function in units of 1 km/s/Mpc. 'Z' stands for (1+z)
         return Ho*(Ω_r*Z**4+Ω_m*Z**3+Ω_Λ)**0.5
 
 
-A=0.019*1e-2*1e-4   #This is the product of fduty, fx, λ, fpbh.
-n=-2.6              #Spectral index
+A=0.019*1e-2*1e-4   #This is the product of fduty, (fx. λ)^0.86 & fpbh. This value of 'A' generates just the right amount of ERB
+#to explain the EDGES signal, which is ~5% of the ARCADE2/LWA1 limit. See arXiv:2107.02190.
+
+n=-2.6              #Spectral index of the ERB
 
 def ERB(E):
         return Tγo+0.0541*24.1*(E/1.282)**-2.6
@@ -38,7 +41,7 @@ def ERB1(E):
 
 E=np.logspace(-1,2)
 J=(E/5.9)**(n+2)*A*ρdm*4.12e46*scint.quad(lambda Z:Z**(n+1)/H(Z),1,np.inf)[0]
-T=9.5e-61*J/(E*1e-6*1.6e-19)**2
+T=9.5e-61*J/(E*1e-6*1.6e-19)**2 #This excess radio temperature explains the 5% limit of ARCADE2/LWA1, as explained in line 26 & 27.
 
 fig,ax=plt.subplots()
 
